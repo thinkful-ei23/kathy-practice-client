2fa668e678c6bc885288b9299a479b5589554f1d
kathyleboeuf <kleboeuf@townisp.com>
2018-09-10 10:56:13 -0400

    added add-list, list, and card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 //import CheeseList from './components/cheese-list.js';
 import AddList from './components/add-list.js';
 import List from './components/list.js';
+import Card from './components/card.js';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="working">
         <AddList />
         <List />
+        <Card />
       </div>
     )
   }
--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './card.css';
-
+import List from './list.js';
+import AddList from './add-list.js';
 
 export default function Card(props) {
 
 	return (
+		<div className="card">
+			<ul>
+				<li className="title">Title</li>
+				<li>{props.list}</li>
+				<li><List /></li>
+				<li><AddList /></li>
+			</ul>
+		</div>
 
-
-
-
-		);
-
-
-
-
-
-
-
-
-
+	);
 
 };
